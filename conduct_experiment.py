16e97ba07f1dc42fb7d9cd150f271e81eab3c49f
Elton Lam <elton.lam@ahs.ca>
2023-04-18 14:21:31 -0600

    changed csv writer output location

--- conduct_experiment.py
+++ conduct_experiment.py
@@ -2,16 +2,15 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import threading
-import csv
 import keyboard
 import paho.mqtt.client as mqtt
 import json
 import ssl
 import datetime
-from time import localtime, strftime
+from modules.csv import setup_csv
 import time
-from pathlib import Path
 
+tag1_csv_writer, tag2_csv_writer = setup_csv()
 class StartThread(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
@@ -31,24 +30,6 @@ class StartThread(threading.Thread):
             elif keyboard.is_pressed('q'):
                 client.loop_stop()
                 quit()
-
-# setup csv output
-csv_dir = os.path.join(Path(r"C:\Users\ML-2\Documents\GitHub\UWBE"),
-                       "csv",
-                       f"{datetime.date.today().strftime('%Y-%m-%d')}",
-                       "Experiments")
-if not os.path.exists(csv_dir):
-    os.makedirs(csv_dir)
-#Create output csv
-tag1_csv = os.path.join(csv_dir, f'10001009_T{strftime("%H:%M:%S", localtime()).replace(":", "-")}.csv')
-tag2_csv = os.path.join(csv_dir, f'10001001_T{strftime("%H:%M:%S", localtime()).replace(":", "-")}.csv')
-
-tag1_csv_file = open(tag1_csv, 'w', newline='')
-tag1_csv_writer = csv.writer(tag1_csv_file, dialect='excel')
-tag1_csv_writer.writerow(['tag_id', 'x', 'y', 'z', 'zone', 'moving', 'success', 'current_time'])
-tag2_csv_file = open(tag2_csv, 'w', newline='')
-tag2_csv_writer = csv.writer(tag2_csv_file, dialect='excel')
-tag2_csv_writer.writerow(['tag_id', 'x', 'y', 'z', 'zone', 'moving', 'success', 'current_time'])
 
 #setup connection
 tenant_id = "63ee6a286f3dc2ff641a73e2"
